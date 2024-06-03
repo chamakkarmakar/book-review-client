9f731879ccec6c9775f32a6b58752401019160fd
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-06-03 16:21:24 +0600

    make footer section

--- src/Components/CommonSection/Footer.jsx
+++ src/Components/CommonSection/Footer.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+
+    <footer className="flex flex-col text-black">
+      <div className="flex justify-around items-center bg-sky-50 py-10">
+        <aside className="text-xl">
+         
+          <p>BookWorm</p>
+        </aside>
+        <nav className="flex flex-col gap-3 text-lg">
+          <ul>
+            <li>
+              <a className="cursor-pointer hover:underline">Home</a>
+            </li>
+            <li>
+              <a className="cursor-pointer hover:underline">Contact</a>
+            </li>
+            <li>
+              <a className="cursor-pointer hover:underline">Books</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <aside className="bg-sky-100 py-5 text-center text-sm">
+        <p>&copy; 2024 BookWorm. All Rights Reserved.</p>
+      </aside>
+    </footer>
+
   )
 }
 
