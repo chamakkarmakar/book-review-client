c90cec03609190489e4c7088dc215b91308d1189
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-06-05 11:29:39 +0600

    some update in home pg Reviews section

--- src/Components/BookReviews/Reviews.jsx
+++ src/Components/BookReviews/Reviews.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import { ImQuotesRight } from "react-icons/im";
+import { MdOutlineStarRate } from "react-icons/md";
 
-const Reviews = ({book}) => {
-    const {title,book_image,author,review_text,ratings,category}=book
+const Reviews = ({ book }) => {
+  const { title, book_image, author, review_text, ratings, category } = book
   return (
-    <div className="mx-auto my-20 w-[350px] space-y-6 rounded-xl bg-white px-4 pb-8 pt-4 font-sans shadow-lg dark:bg-[#18181B]">
-        <div className="flex h-44 w-full justify-center lg:h-[250px]">
-            <img width={300} height={300} className="h-full w-full rounded-lg bg-black/40" src={book_image} alt={title} />
+    <div className="p-4 min-w-full md:min-w-[50%] ">
+      <div className="h-full p-8 rounded-lg shadow-[0px_4px_12px_rgba(0,0,0,0.1)] bg-white">
+        <div className="flex justify-between items-center">
+          <ImQuotesRight className='w-10 h-10 text-sky-800' />
+          <p className='flex items-center'>
+            <MdOutlineStarRate className='w-5 h-5 text-amber-500 inline' />
+            <MdOutlineStarRate className='w-5 h-5 text-amber-500 inline' />
+            <MdOutlineStarRate className='w-5 h-5 text-amber-500 inline' />
+            <MdOutlineStarRate className='w-5 h-5 text-amber-500 inline' />
+            <MdOutlineStarRate className='w-5 h-5 text-amber-500 inline' />
+            <span className='ml-3 '>{ratings}</span>
+          </p>
         </div>
-        <div className="mx-auto w-[85%] space-y-2 text-center font-semibold">
-            <h6 className="text-sm md:text-base lg:text-lg">{title}</h6>
-            <p className="text-xs font-semibold text-gray-400 md:text-sm">{author}</p>
-        </div>
-        <p className="text-xs font-semibold text-gray-700 md:text-sm">"{review_text}"</p>
-        <div className='flex justify-between items-center'>
-        <p className="text-xs font-semibold text-gray-700 md:text-sm">ratings: {ratings}</p>
-        <p className="text-xs font-semibold text-gray-400 md:text-sm"> {category}</p>
-        </div>
-
-        <div className="flex flex-wrap items-center justify-center gap-6 text-sm md:text-base">
-        <button className="t w-[80%] rounded-full py-2 font-medium text-gray-400 shadow-[0px_0px_10px_#E2DADA] duration-500  hover:scale-95 hover:bg-[#0095FF] hover:text-white hover:shadow-xl dark:shadow-[0px_2px_8px_0px_rgba(0,0,0,0.8)]">View Details</button>
-        </div>
+        <p className="leading-relaxed mb-6 text-gray-500">{review_text}</p>
+        <a className="inline-flex items-center">
+          <img className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" src={book_image} alt="carousel navigate ui" />
+          <span className="flex-grow flex flex-col pl-4">
+            <span className="title-font font-medium text-gray-900">{title}</span>
+            <span className="text-gray-500 text-sm">{author}</span>
+          </span>
+        </a>
+      </div>
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,29 +13,64 @@ const Home = () => {
     }
     load();
   }, [])
+
+  const [currentSlider, setCurrentSlider] = useState(0);
+
+  const prevSlider = () => setCurrentSlider((currentSlider) => (currentSlider === 0 ? books.length - 2 : currentSlider - 1));
+  const nextSlider = () => setCurrentSlider((currentSlider) => (currentSlider === books.length - 2 ? 0 : currentSlider + 1));
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlider();
+    }, 3000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentSlider]);
+
+  const isSmallScreen = window.innerWidth <= 768;
+
   return (
     <div>
       <Banner />
-      <div className='my-20'>
+      <div className='py-20'>
         <h1 className='text-center text-5xl font-semibold font-serif'>Popular Books</h1>
         <div className='grid md:grid-cols-3 grid-cols-1 gap-6'>
           {
-            books.map((book,index) =>
+            books.map((book, index) =>
               <Book key={index} book={book} />
             )
           }
 
         </div>
       </div>
-      <div className='my-20'>
+      <div className='py-20 bg-sky-50'>
         <h1 className='text-center text-5xl font-semibold font-serif'>Reviews</h1>
-        <div className='grid md:grid-cols-2 grid-cols-1 gap-6'>
-          {
-            books.map((book,index) =>
-              <Reviews key={index} book={book} />
-            )
-          }
-
+        <div className="max-w-full min-w-[350px]  mx-auto h-[400px] flex flex-row items-center overflow-hidden gap-5 lg:gap-10 px-8 md:px-16 lg:px-24">
+          <div className="relative overflow-hidden">
+            <div className="absolute w-full h-full flex items-center justify-between z-50 px-5">
+              {/* arrow left */}
+              <button onClick={prevSlider} className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8">
+                <svg viewBox="0 0 1024 1024" className="w-4 h-4 md:w-6 md:h-6 icon" xmlns="http://www.w3.org/2000/svg" fill="black"> <g strokeWidth="0"></g> <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g> <g id="SVGRepo_iconCarrier"><path fill="black" d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"></path></g>
+                </svg>
+              </button>
+              {/* arrow right */}
+              <button onClick={nextSlider} className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8">
+                <svg viewBox="0 0 1024 1024" className="w-4 h-4 md:w-6 md:h-6 icon" xmlns="http://www.w3.org/2000/svg" fill="black" transform="rotate(180)"> <g strokeWidth="0"></g> <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g> <g id="SVGRepo_iconCarrier"> <path fill="black" d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z" ></path></g>
+                </svg>
+              </button>
+            </div>
+            {/* slider container */}
+            <div
+              className="ease-linear duration-300 flex"
+              style={{ transform: `translateX(-${currentSlider * (isSmallScreen ? 100 : 50)}%)` }}>
+              {
+                books.map((book, index) =>
+                  <Reviews key={index} book={book} />
+                )
+              }
+            </div>
+          </div>
         </div>
       </div>
     </div>
